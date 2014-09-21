797e587e0dc4fd48b8c2b01a50ae52a99442ae8d
osigachev <osigachev@users.noreply.github.com>
2014-09-21 16:23:12 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse (MatrInv) of a matrix (Matr) instead 
+## of computing it repeatedly unless the matrix (Matr) is changed
 
-## Write a short comment describing this function
+## The following allows to get or modify the value of matrix (Matr)
+## and to get or realculate the value of inverse (MatrInv)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(Matr = matrix()) {
+        MatrInv <- NULL
+        set <- function(X) {
+                Matr <<- X
+                MatrInv <<- NULL
+        }
+        get <- function() Matr
+        setInvMatr <- function(X) MatrInv <<- X
+        getInvMatr <- function() MatrInv
+        list(set = set, get = get, 
+             setInvMatr = setInvMatr, 
+             getInvMatr = getInvMatr)        
 }
 
+## The following prints cashed inverse (MatrInv) of the matrix (Matr) or
+## recalculates the inverse if the matrix has been changed
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(Matr, ...) {
+        MatrInv <- Matr$getInvMatr()
+        if (is.null(MatrInv)) {
+                message('Getting Cached Data...')
+                data <- Matr$get()
+                MatrInv <- solve(data, ...)
+                Matr$setInvMatr(MatrInv)
+        } else {
+                message('Printing Cached Inverse...')
+        }
+        return(MatrInv)
 }
